62102027999202fe3e0427240c94c3062ec59d28
srinivasallanki <srinivas5859@gmail.com>
2022-04-10 20:39:54 +0530

    Custom Dropdown with events and keys

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [isOptionsOpen, setIsOptionsOpen] = useState(false)
+  const [selectedOption, setSelectedOption] = useState(0)
+  let timeOutId = null
+
+  const optionsList = [
+    'Option 1',
+    'Option 2',
+    'Option 3',
+    'Option 4',
+    'Option 5',
+  ]
+
+  const toggleOptions = () => {
+    setIsOptionsOpen(!isOptionsOpen)
+  }
+
+  const setSelectedThenCloseDropdown = (index) => {
+    setSelectedOption(index)
+    setIsOptionsOpen(false)
+  }
+
+  const handleKeyDown = (index) => (e) => {
+    switch (e.key) {
+      case ' ':
+      case 'SpaceBar':
+      case 'Enter':
+        e.preventDefault()
+        setSelectedThenCloseDropdown(index)
+        break
+      default:
+        break
+    }
+  }
+
+  const handleListKeyDown = (e) => {
+    switch (e.key) {
+      case 'Escape':
+        e.preventDefault()
+        setIsOptionsOpen(false)
+        break
+      case 'ArrowUp':
+        e.preventDefault()
+        setSelectedOption(
+          selectedOption - 1 >= 0 ? selectedOption - 1 : optionsList.length - 1
+        )
+        break
+      case 'ArrowDown':
+        e.preventDefault()
+        setSelectedOption(
+          selectedOption == optionsList.length - 1 ? 0 : selectedOption + 1
+        )
+        break
+      default:
+        break
+    }
+  }
+
+  const handlerBlue = () => {
+    timeOutId = setTimeout(() => {
+      setIsOptionsOpen(false)
+    })
+  }
+
+  const handlerFocus = () => {
+    clearTimeout(timeOutId)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='wrapper'>
+      <div className='container' onFocus={handlerFocus} onBlur={handlerBlue}>
+        <button
+          type='button'
+          aria-haspopup='listbox'
+          aria-expanded={isOptionsOpen}
+          className={isOptionsOpen ? 'expanded' : ''}
+          onClick={toggleOptions}
+          onKeyDown={handleListKeyDown}
         >
-          Learn React
-        </a>
-      </header>
+          {optionsList[selectedOption]}
+        </button>
+        <ul
+          className={`options ${isOptionsOpen ? 'show' : ''}`}
+          role='listbox'
+          aria-activedescendant={optionsList[selectedOption]}
+          tabIndex={-1}
+          onKeyDown={handleListKeyDown}
+        >
+          {optionsList.map((option, index) => (
+            <li
+              id={option}
+              role='option'
+              aria-selected={selectedOption == index}
+              tabIndex={0}
+              onKeyDown={handleKeyDown(index)}
+              onClick={() => {
+                setSelectedThenCloseDropdown(index)
+              }}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
